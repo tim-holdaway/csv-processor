f5a45f6a822dc3fc70179ad3e806f49850d9467f
Tim Holdaway <timholdaway.dev@gmail.com>
2023-10-21 08:07:25 -0700

    basic implementation of file processor

--- build.gradle.kts
+++ build.gradle.kts
@@ -11,6 +11,7 @@ repositories {
 }
 
 dependencies {
+    implementation("com.fasterxml.jackson.dataformat:jackson-dataformat-csv:2.15.3")
     testImplementation(platform("org.junit:junit-bom:5.9.1"))
     testImplementation("org.junit.jupiter:junit-jupiter")
 }
---
+++ src/main/java/com/timholdaway/FileProcessor.java
@@ -0,0 +1,27 @@
+package com.timholdaway;
+
+import com.fasterxml.jackson.databind.MappingIterator;
+import com.fasterxml.jackson.dataformat.csv.CsvMapper;
+import com.fasterxml.jackson.dataformat.csv.CsvSchema;
+
+import java.io.File;
+import java.io.IOException;
+
+public class FileProcessor {
+
+    record InputRow (String fname, String lname, int age) {}
+    public void processFile(File file) throws IOException {
+        CsvMapper mapper = new CsvMapper();
+        CsvSchema headerSchema = CsvSchema.emptySchema().withHeader();
+
+        try (MappingIterator<InputRow> it = mapper.readerFor(InputRow.class)
+                .with(mapper.schemaFor(InputRow.class))
+                .with(headerSchema)
+                .readValues(file)) {
+            while (it.hasNext()) {
+                InputRow current = it.next();
+                System.out.println("Read a row: " + current);
+            }
+        }
+    }
+}
