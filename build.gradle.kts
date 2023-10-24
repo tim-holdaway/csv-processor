b0512a2add84b733cec298561565f9ba6d84354a
Tim Holdaway <timholdaway.dev@gmail.com>
2023-10-23 21:15:11 -0700

    add assertion library to build.gradle

--- build.gradle.kts
+++ build.gradle.kts
@@ -15,6 +15,8 @@ dependencies {
     implementation("com.fasterxml.jackson.dataformat:jackson-dataformat-csv:2.15.3")
     testImplementation(platform("org.junit:junit-bom:5.9.1"))
     testImplementation("org.junit.jupiter:junit-jupiter")
+    testImplementation("org.assertj:assertj-core:3.24.2")
+
 }
 
 tasks.test {
